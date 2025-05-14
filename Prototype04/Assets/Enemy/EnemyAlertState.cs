1f07093d45719adccd193b31fda425056d25bd9d
cunxigao <cg4352@nyu.edu>
2025-05-14 16:10:50 -0400

    三个场地敌人最终设置 / collider完成

--- Prototype04/Assets/Enemy/EnemyAlertState.cs
+++ Prototype04/Assets/Enemy/EnemyAlertState.cs
@@ -12,15 +12,7 @@ public override void EnterState(EnemyStateManager enemy)
     
     public override void UpdateState(EnemyStateManager enemy)
     {
-        // 使用敌人当前的前方向作为基础旋转
-        Quaternion baseRotation = Quaternion.LookRotation(enemy.transform.forward);
-        
-        // 应用抖动效果
-        Quaternion targetRotation = enemy.GetJitteredRotation(baseRotation);
-        
-        // 应用旋转
-        float rotationSpeed = 5f;
-        enemy.transform.rotation = Quaternion.Slerp(enemy.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+       
     }
     
     public override void ExitState(EnemyStateManager enemy)
